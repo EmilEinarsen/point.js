f7a5dd4dc20a3ce2c293246f7be7cf72f5251aeb
EmilEinarsen <emil@hagatun.se>
2022-02-13 23:58:12 +0100

    docs: added comments for basic arithmetical methods & rounders

--- src/index.ts
+++ src/index.ts
@@ -57,22 +57,160 @@ class P {
 		this.#assign(this.#get(...args))
 	}
 
+	/**
+	 * Adds addends to `this`, mutating `this` to the sum.
+	 *
+	 * @param {...*} addends addends in the addition.
+	 * @return {this} `this`
+	 * @chainable
+	 * @example
+	 * 
+	 * new P(10).add(2,5)
+	 * // => P { x: 12, y: 15 }
+	 */
 	add = this.#arithWrapper((a,b) => a + (b??0))
+
+	/**
+	 * Adds addends, instantiating `P` by the sum.
+	 *
+	 * @param {...*} addends addends in the addition.
+	 * @return {P} `P`
+	 * @chainable
+	 * @example
+	 * 
+	 * P.add(10,[2,5])
+	 * // => P { x: 12, y: 15 }
+	 */
 	static add = this.staticArithWrapper('add')
 
+	/**
+	 * Subtracts subtrahends from `this`, mutating `this` to the difference.
+	 *
+	 * @param {...*} subtrahends subtrahends in the subtraction.
+	 * @return {this} `this`
+	 * @chainable
+	 * @example
+	 * 
+	 * new P(10).sub([2,5])
+	 * // => P { x: 8, y: 5 }
+	 */
 	sub = this.#arithWrapper((a,b) => a - (b??0))
+
+	/**
+	 * Subtracts subtrahends, instantiating `P` by the difference.
+	 *
+	 * @param {...*} subtrahends subtrahends in the subtraction.
+	 * @return {P} `P`
+	 * @chainable
+	 * @example
+	 * 
+	 * P.sub(10,[2,5])
+	 * // => P { x: 8, y: 5 }
+	 */
 	static sub = this.staticArithWrapper('sub')
 
+	/**
+	 * Multiplies `this` by multiplicands, mutating `this` to the product.
+	 *
+	 * @param {...*} multiplicands multiplicands in the multiplication.
+	 * @return {this} `this`
+	 * @chainable
+	 * @example
+	 * 
+	 * new P(10).mult([2,5])
+	 * // => P { x: 20, y: 50 }
+	 */
 	mult = this.#arithWrapper((a,b) => a * (b??1))
+
+	/**
+	 * Multiplies multiplicands, instantiating `P` by the product.
+	 *
+	 * @param {...*} multiplicands multiplicands in the multiplication.
+	 * @return {P} `P`
+	 * @chainable
+	 * @example
+	 * 
+	 * P.mult(10,[2,5])
+	 * // => P { x: 20, y: 50 }
+	 */
 	static mult = this.staticArithWrapper('mult')
 
+	/**
+	 * Divides `this` by divisors, mutating `this` to the quotient.
+	 *
+	 * @param {...*} divisors divisors in the division.
+	 * @return {this} `this`
+	 * @chainable
+	 * @example
+	 * 
+	 * new P(10).div([2,5])
+	 * // => P { x: 5, y: 2 }
+	 */
 	div = this.#arithWrapper((a,b) => a / (b??1))
+
+	/**
+	 * Divides divisors, instantiating `P` by the product.
+	 *
+	 * @param {...*} divisors divisors in the division.
+	 * @return {P} `P`
+	 * @chainable
+	 * @example
+	 * 
+	 * P.div(10,[2,5])
+	 * // => P { x: 5, y: 2 }
+	 */
 	static div = this.staticArithWrapper('div')
 
+	/**
+	 * Divides `this` by divisors, mutating `this` to the modulus.
+	 *
+	 * @param {...*} divisors divisors in the division.
+	 * @return {this} `this`
+	 * @chainable
+	 * @example
+	 * 
+	 * new P(10).mod([2,4])
+	 * // => P { x: 0, y: 2 }
+	 */
 	mod = this.#arithWrapper((a,b) => a % (b??0))
+
+	/**
+	 * Divides divisors, instantiating `P` by the modulus.
+	 *
+	 * @param {...*} divisors divisors in the division.
+	 * @return {P} `P`
+	 * @chainable
+	 * @example
+	 * 
+	 * P.mod(10,[2,4])
+	 * // => P { x: 0, y: 2 }
+	 */
 	static mod = this.staticArithWrapper('mod')
 
+	/**
+	 * Exponentiates `this` by exponents, mutating `this` to the product.
+	 *
+	 * @param {...*} exponents exponents in the exponentiation.
+	 * @return {this} `this`
+	 * @chainable
+	 * @example
+	 * 
+	 * new P(10).pow([2,4])
+	 * // => P { x: 100, y: 10000 }
+	 */
 	pow = this.#arithWrapper((a,b) => a ** (b??1))
+
+	/**
+	 * Exponentiates exponents, instantiating `P` by the product.
+	 *
+	 * @param {...*} exponents exponents in the exponentiation.
+	 * @return {P} `P`
+	 * @chainable
+	 * @example
+	 * 
+	 * P.pow(10,[2,4])
+	 * // => P { x: 100, y: 10000 }
+	 */
 	static pow = this.staticArithWrapper('pow')
 
 
@@ -98,9 +236,56 @@ class P {
 	min
 	max */
 
+	/**
+	 * Calculates `this` rounded up to precision.
+	 *
+	 * @param precision The precision to round down to.
+	 * @return {this} `this`
+	 * @chainable
+	 * @example
+	 * 
+	 * new P(-5.2, 2.9).ceil()
+	 * // => P { x: -5, y: 3 }
+	 */
 	ceil = this.#sideEffectWrapper(createRound(Math.ceil))
-	round = this.#sideEffectWrapper(createRound(Math.round))
+
+	/**
+	 * Calculates `this` rounded down to precision.
+	 *
+	 * @param precision The precision to round down to.
+	 * @return {this} `this`
+	 * @chainable
+	 * @example
+	 * 
+	 * new P(-5.2, 2.9).floor()
+	 * // => P { x: -6, y: 2 }
+	 */
 	floor = this.#sideEffectWrapper(createRound(Math.floor))
+
+	/**
+	 * Calculates `this` rounded to precision.
+	 *
+	 * @param precision The precision to round down to.
+	 * @return {this} `this`
+	 * @chainable
+	 * @example
+	 * 
+	 * new P(-5.2, 2.9).round()
+	 * // => P { x: 5, y: 3 }
+	 */
+	round = this.#sideEffectWrapper(createRound(Math.round))
+
+	/**
+	 * Calculates `this` rounded towards 0 to precision.
+	 *
+	 * @param precision The precision to round down to.
+	 * @return {this} `this`
+	 * @chainable
+	 * @example
+	 * 
+	 * new P(-5.2, 2.9).trunc()
+	 * // => P { x: -5, y: 2 }
+	 */
 	trunc = this.#sideEffectWrapper(createRound(Math.trunc))
 
 	random = this.#wrapper(random)
